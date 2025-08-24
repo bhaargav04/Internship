d15ef9949fded32fc7a0e47aa2a7ba747ffcc82a
Bhaargav <bhaargav04@gmail.com>
2025-08-24 22:25:18 +0530

    hr_document

--- Django/TaxCalculator/Tds/templatetags/customfilters.py
+++ Django/TaxCalculator/Tds/templatetags/customfilters.py
@@ -6,3 +6,8 @@ register = template.Library()
 @register.filter
 def get_item(dictionary, key):
     return dictionary.get(key)
+
+@register.filter
+def endswith(value, arg):
+    """Check if value ends with arg."""
+    return str(value).lower().endswith(str(arg).lower())
